8bf338014956e18c0fef31ec31a9f0b83b4dd844
markMe1024 <com.mark.yu2015@gmail.com>
2026-03-25 19:13:43 +0800

    test

---
+++ test.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    """返回两个数的和。
+    
+    Args:
+        a: 第一个数
+        b: 第二个数
+    
+    Returns:
+        两个数的和
+    """
+    return a + b
+
+
+def subtract(a, b):
+    """返回两个数的差。
+    
+    Args:
+        a: 被减数
+        b: 减数
+    
+    Returns:
+        两个数的差 (a - b)
+    """
+    return a - b
