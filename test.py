1234033847915f3aff5d762e8a25fa4036ad48e5
markMe1024 <com.mark.yu2015@gmail.com>
2026-03-25 19:27:13 +0800

    t

--- test.py
+++ test.py
@@ -22,3 +22,32 @@ def subtract(a, b):
         两个数的差 (a - b)
     """
     return a - b
+
+
+def multiply(a, b):
+    """返回两个数的乘积。
+    
+    Args:
+        a: 第一个数
+        b: 第二个数
+    
+    Returns:
+        两个数的乘积 (a * b)
+    """
+    return a * b
+
+
+def divide(a, b):
+    """返回两个数的商。
+    
+    Args:
+        a: 被除数
+        b: 除数
+    
+    Returns:
+        两个数的商 (a / b)
+    
+    Raises:
+        ZeroDivisionError: 当除数为0时抛出
+    """
+    return a / b
